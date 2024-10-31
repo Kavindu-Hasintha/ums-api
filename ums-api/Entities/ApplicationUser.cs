c3c345b46e48d38c3ec28e8a9c12f443e5606e15
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2024-10-31 23:38:51 +0530

    Application user entity added

--- ums-api/Entities/ApplicationUser.cs
+++ ums-api/Entities/ApplicationUser.cs
@@ -1,6 +1,16 @@
-﻿namespace ums_api.Entities
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ums_api.Entities
 {
-    public class ApplicationUser
+    public class ApplicationUser: IdentityUser
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        [NotMapped]
+        public IList<string> Roles { get; set; }
     }
 }
