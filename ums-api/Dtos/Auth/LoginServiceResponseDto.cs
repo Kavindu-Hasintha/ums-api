c2f22fe1cf36e1a0a198a0847ed606a10db5b30c
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2024-11-12 10:41:18 +0530

    LoginServiceResponseDto added

---
+++ ums-api/Dtos/Auth/LoginServiceResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ums_api.Dtos.Auth
+{
+    public class LoginServiceResponseDto
+    {
+        public string NewToken { get; set; }
+
+        // This would be returned to front-end
+        public int MyProperty { get; set; }
+    }
+}
