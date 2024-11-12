468dc13bac9dc25f0455396f750169cc5245da73
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2024-11-12 10:25:56 +0530

    Add json options to Program file

--- ums-api/Program.cs
+++ ums-api/Program.cs
@@ -1,11 +1,17 @@
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 using ums_api.DbContext;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
 
-builder.Services.AddControllers();
+builder.Services
+    .AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
 
 
 // DB
