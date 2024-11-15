4051e6e3c0b3ecbbd63939126391273910919ad0
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2024-11-15 14:13:14 +0530

    General service response dto added

---
+++ ums-api/Dtos/General/GeneralServiceResponseDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ums_api.Dtos.General
+{
+    public class GeneralServiceResponseDto
+    {
+        public bool IsSucceed { get; set; }
+
+        public int StatusCode { get; set; }
+
+        public string Message { get; set; }
+    }
+}
