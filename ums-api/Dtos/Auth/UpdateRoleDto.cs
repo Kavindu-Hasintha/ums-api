6286045cf621bc329922c6245a5c3d5b8e6170ad
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2024-11-15 14:09:57 +0530

    UpdateRoleDto added

---
+++ ums-api/Dtos/Auth/UpdateRoleDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ums_api.Dtos.Auth
+{
+    public class UpdateRoleDto
+    {
+        [Required(ErrorMessage = "Username is required")]
+        public string Username { get; set; }
+
+        public RoleType NewRole { get; set; }
+    }
+
+    public enum RoleType
+    {
+        ADMIN,
+        MANAGER,
+        USER
+    }
+}
