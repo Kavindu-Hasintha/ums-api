6523a4b08393aff7d6be9fd22ae39ea0beb0d510
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2024-11-15 15:05:51 +0530

    CreateMessageDto added

---
+++ ums-api/Dtos/Message/CreateMessageDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ums_api.Dtos.Message
+{
+    public class CreateMessageDto
+    {
+        public string ReceiverUsername { get; set; }
+
+        public string Text { get; set; }
+    }
+}
