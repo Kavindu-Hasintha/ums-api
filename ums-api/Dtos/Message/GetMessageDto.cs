6ba8f05dc9f977ff6c06f2657f89bcf6a87ff776
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2024-11-15 15:08:55 +0530

    GetMessageDto added

---
+++ ums-api/Dtos/Message/GetMessageDto.cs
@@ -0,0 +1,15 @@
+﻿namespace ums_api.Dtos.Message
+{
+    public class GetMessageDto
+    {
+        public long id { get; set; }
+
+        public string SenderUsername { get; set; }
+
+        public string ReceiverUsername { get; set; }
+
+        public string Text { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
