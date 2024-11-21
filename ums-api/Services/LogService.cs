0d9d7bf265730160586f763a2c7830f1030eba67
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2024-11-22 00:04:08 +0530

    LogService added

---
+++ ums-api/Services/LogService.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.Claims;
+using ums_api.DbContext;
+using ums_api.Dtos.Log;
+using ums_api.Interfaces;
+
+namespace ums_api.Services
+{
+    public class LogService : ILogService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LogService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public Task SaveNewLog(string Username, string Description)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<GetLogDto>> GetLogsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<GetLogDto>> GetMyLogsAsync(ClaimsPrincipal User)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
