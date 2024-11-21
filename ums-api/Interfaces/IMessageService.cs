2807df2c6cd5649ddbd5ebdd4dda2170ea426b64
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2024-11-21 23:46:45 +0530

    IMessageService added

---
+++ ums-api/Interfaces/IMessageService.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Claims;
+using ums_api.Dtos.General;
+using ums_api.Dtos.Message;
+
+namespace ums_api.Interfaces
+{
+    public interface IMessageService
+    {
+        Task<GeneralServiceResponseDto> CreateNewMessageAsync(ClaimsPrincipal User, CreateMessageDto createMessageDto);
+        Task<IEnumerable<GetMessageDto>> GetMessagesAsync();
+        Task<IEnumerable<GetMessageDto>> GetMyMessagesAsync(ClaimsPrincipal User);
+    }
+}
