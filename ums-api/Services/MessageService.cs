a7350bc01e1e51ab0efa114a13eb1416022e873c
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2024-11-28 10:57:13 +0530

    MessageService class added

---
+++ ums-api/Services/MessageService.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+using ums_api.DbContext;
+using ums_api.Dtos.General;
+using ums_api.Dtos.Message;
+using ums_api.Entities;
+using ums_api.Interfaces;
+
+namespace ums_api.Services
+{
+    public class MessageService : IMessageService
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogService _logService;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public MessageService(ApplicationDbContext context, ILogService logService, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _logService = logService;
+            _userManager = userManager;
+        }
+
+        public Task<GeneralServiceResponseDto> CreateNewMessageAsync(ClaimsPrincipal User, CreateMessageDto createMessageDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<GetMessageDto>> GetMessagesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<GetMessageDto>> GetMyMessagesAsync(ClaimsPrincipal User)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
