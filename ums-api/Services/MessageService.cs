d343be42cca0ce598a4575fa8725faab8f28f609
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2024-11-28 11:23:48 +0530

    CreateNewMessageAsync function added

--- ums-api/Services/MessageService.cs
+++ ums-api/Services/MessageService.cs
@@ -21,9 +21,45 @@ namespace ums_api.Services
             _userManager = userManager;
         }
 
-        public Task<GeneralServiceResponseDto> CreateNewMessageAsync(ClaimsPrincipal User, CreateMessageDto createMessageDto)
+        public async Task<GeneralServiceResponseDto> CreateNewMessageAsync(ClaimsPrincipal User, CreateMessageDto createMessageDto)
         {
-            throw new NotImplementedException();
+            if (User.Identity.Name == createMessageDto.ReceiverUsername)
+            {
+                return new GeneralServiceResponseDto()
+                {
+                    IsSucceed = false,
+                    StatusCode = 400,
+                    Message = "Sender and Receiver can not be same",
+                };
+            }
+
+            var isReceiverUsernameValid = _userManager.Users.Any(q => q.UserName == createMessageDto.ReceiverUsername);
+            if (!isReceiverUsernameValid)
+            {
+                return new GeneralServiceResponseDto()
+                {
+                    IsSucceed = false,
+                    StatusCode = 400,
+                    Message = "Receiver Username is not valid",
+                };
+            }
+
+            Message newMessage = new Message()
+            {
+                SenderUserName = User.Identity.Name,
+                ReceiverUserName = createMessageDto.ReceiverUsername,
+                Text = createMessageDto.Text,
+            };
+            await _context.Messages.AddAsync(newMessage);
+            await _context.SaveChangesAsync();
+            await _logService.SaveNewLog(User.Identity.Name, "Send Message");
+
+            return new GeneralServiceResponseDto()
+            {
+                IsSucceed = true,
+                StatusCode = 201,
+                Message = "Message saved successfully",
+            };
         }
 
         public Task<IEnumerable<GetMessageDto>> GetMessagesAsync()
