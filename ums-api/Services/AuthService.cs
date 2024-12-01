3a6cc30101a6e081ff2bc0a4f9589f5e2f6c6185
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2024-12-02 00:43:14 +0530

    AuthService added

---
+++ ums-api/Services/AuthService.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+using ums_api.Dtos.Auth;
+using ums_api.Dtos.General;
+using ums_api.Entities;
+using ums_api.Interfaces;
+
+namespace ums_api.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly ILogService _logService;
+        private readonly IConfiguration _configuration;
+
+        public AuthService(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, ILogService logService, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _logService = logService;
+            _configuration = configuration;
+        }
+
+        public Task<UserInfoResult> GetUserDetailsByUsername(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<string>> GetUsernamesListAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<UserInfoResult>> GetUsersListAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LoginServiceResponseDto> LoginAsync(LoginDto loginDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LoginServiceResponseDto> MeAsync(MeDto meDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GeneralServiceResponseDto> RegisterAsync(RegisterDto registerDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GeneralServiceResponseDto> SeedRolesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GeneralServiceResponseDto> UpdateRoleAsync(ClaimsPrincipal User, UpdateRoleDto updateRoleDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
