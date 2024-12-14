f8fa233ddb927f1c9dad3248b6e93b3570b7bb5a
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2024-12-14 12:06:52 +0530

    GetLogs for owner/admin API added

--- ums-api/Controllers/LogsController.cs
+++ ums-api/Controllers/LogsController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ums_api.Constants;
+using ums_api.Dtos.Log;
+using ums_api.Interfaces;
 
 namespace ums_api.Controllers
 {
@@ -7,5 +11,19 @@ namespace ums_api.Controllers
     [ApiController]
     public class LogsController : ControllerBase
     {
+        private readonly ILogService _logService;
+
+        public LogsController(ILogService logService)
+        {
+            _logService = logService;
+        }
+
+        [HttpGet]
+        [Authorize(Roles = StaticUserRoles.OwnerAdmin)]
+        public async Task<ActionResult<IEnumerable<GetLogDto>>> GetLogs()
+        {
+            var logs = await _logService.GetLogsAsync();
+            return Ok(logs);
+        }
     }
 }
