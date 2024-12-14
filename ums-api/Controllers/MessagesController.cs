2405350bc6854ff27951d5319e05e23bb54c5fa1
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2024-12-14 12:38:59 +0530

    GetMyMessages API added

--- ums-api/Controllers/MessagesController.cs
+++ ums-api/Controllers/MessagesController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ums_api.Dtos.Message;
 using ums_api.Interfaces;
 
 namespace ums_api.Controllers
@@ -15,6 +17,30 @@ namespace ums_api.Controllers
             _messageService = messageService;
         }
 
+        // Route -> Create a new message to send to another user
+        [HttpPost]
+        [Route("create")]
+        [Authorize]
+        public async Task<IActionResult> CreateNewMessage([FromBody]CreateMessageDto createMessageDto)
+        {
+            var result = await _messageService.CreateNewMessageAsync(User, createMessageDto);
 
+            if (result.IsSucceed)
+            {
+                return Ok(result.Message);
+            }
+
+            return StatusCode(result.StatusCode, result.Message);
+        }
+
+        // Route -> Get all messages for current user, Either as Sender or as Receiver
+        [HttpGet]
+        [Route("mine")]
+        [Authorize]
+        public async Task<ActionResult<IEnumerable<GetMessageDto>>> GetMyMessages()
+        {
+            var messages = await _messageService.GetMyMessagesAsync(User);
+            return Ok(messages);
+        }
     }
 }
