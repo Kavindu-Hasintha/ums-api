f9471ccbc276630e4bfc9bfffa8fed369f02fbac
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2024-12-14 11:40:55 +0530

    Testing API for GetPublicData, GetUserData APIs added

--- ums-api/Controllers/TestController.cs
+++ ums-api/Controllers/TestController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ums_api.Constants;
 
 namespace ums_api.Controllers
 {
@@ -7,5 +9,19 @@ namespace ums_api.Controllers
     [ApiController]
     public class TestController : ControllerBase
     {
+        [HttpGet]
+        [Route("get-public")]
+        public IActionResult GetPublicData()
+        {
+            return Ok("Public Data");
+        }
+
+        [HttpGet]
+        [Route("get-user-role")]
+        [Authorize(Roles = StaticUserRoles.USER)]
+        public IActionResult GetUserData()
+        {
+            return Ok("User Role Data");
+        }
     }
 }
