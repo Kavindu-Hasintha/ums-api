509db977db2feb96fe72a3786ce48f79524593b1
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2024-12-18 15:21:38 +0530

    SeedRoles API added

--- ums-api/Controllers/AuthController.cs
+++ ums-api/Controllers/AuthController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ums_api.Dtos.Auth;
+using ums_api.Interfaces;
 
 namespace ums_api.Controllers
 {
@@ -7,5 +9,22 @@ namespace ums_api.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        // Route -> Seed Roles to DB
+        [HttpPost]
+        [Route("seed-roles")]
+        public async Task<IActionResult> SeedRoles()
+        {
+            var seedResult = await _authService.SeedRolesAsync();
+            return StatusCode(seedResult.StatusCode, seedResult.Message);
+        }
+
+
     }
 }
