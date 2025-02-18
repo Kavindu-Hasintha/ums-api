2c7721c91917725a2370b15179ab18dc52203af2
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2025-02-18 11:14:22 +0530

    UpdateRole and Me  API implemented

--- ums-api/Controllers/AuthController.cs
+++ ums-api/Controllers/AuthController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ums_api.Constants;
 using ums_api.Dtos.Auth;
 using ums_api.Interfaces;
 
@@ -45,6 +47,47 @@ namespace ums_api.Controllers
                 return Unauthorized("Your credentials are invalid. Please contact to an admin");
             }
             return Ok(loginResult);
+        }
+
+        // Route -> Update User Role
+        // An Owner can change everything
+        // An Admin can change just User to Manager or reverse
+        // Manager and User Roles don't have access to this Route
+        [HttpPost]
+        [Route("update-role")]
+        [Authorize(Roles = StaticUserRoles.OwnerAdmin)]
+        public async Task<IActionResult> UpdateRole([FromBody] UpdateRoleDto updateRoleDto)
+        {
+            var updateRoleResult = await _authService.UpdateRoleAsync(User, updateRoleDto);
+
+            if (updateRoleResult.IsSucceed)
+            {
+                return Ok(updateRoleResult.Message);
+            } else
+            {
+                return StatusCode(updateRoleResult.StatusCode, updateRoleResult.Message);
+            }
+        }
+
+        // Route -> getting data of a user from it's JWT
+        [HttpPost]
+        [Route("me")]
+        public async Task<ActionResult<LoginServiceResponseDto>> Me([FromBody] MeDto token)
+        {
+            try
+            {
+                var me = await _authService.MeAsync(token);
+                if (me is not null)
+                {
+                    return Ok(me);
+                } else
+                {
+                    return Unauthorized("Invalid Token");
+                }
+            } catch (Exception)
+            {
+                return Unauthorized("Invalid Token");
+            }
         }
     }
 }
