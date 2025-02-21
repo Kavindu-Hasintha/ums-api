50a15b18413ed83cf6dd8f511f5c49bb78fa1ccf
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2025-02-21 22:35:56 +0530

    Add cors

--- ums-api/Program.cs
+++ ums-api/Program.cs
@@ -115,6 +115,14 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(options =>
+{
+    options
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .AllowAnyOrigin();
+});
+
 app.UseHttpsRedirection();
 
 app.UseAuthentication();
